7430923c33a8f3627d5ed7c6d7968f34a948efe4
Cabidge <cabbageboi69@gmail.com>
2021-10-13 20:40:00 -0400

    Added delete to prompt subcommands

--- src/commands/prompt.ts
+++ src/commands/prompt.ts
@@ -1,8 +1,9 @@
 import { slashBranch } from "../slash-command";
 import create from "./prompt/create";
+import delete_ from "./prompt/delete";
 
 export default slashBranch({
     name: "prompt",
     description: "",
-    commands: [create],
+    commands: [create, delete_],
 });
