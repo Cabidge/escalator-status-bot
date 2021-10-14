38e40309394884f3f5990ac404691db66da25993
Cabidge <cabbageboi69@gmail.com>
2021-10-13 20:36:41 -0400

    Implemented delete

--- src/commands/prompt/delete.ts
+++ src/commands/prompt/delete.ts
@@ -1,9 +1,24 @@
 import { slashLeaf } from "../../slash-command";
 import { adminOnly } from "../../slash-command/guards";
+import State from "./shared/state";
 
 export default slashLeaf({
     name: "delete",
     description: "Deletes the created prompt",
     guards: [adminOnly],
-    async execute(interaction) {},
+    async execute(interaction) {
+        if (State.status === undefined) {
+            await interaction.reply({
+                content: "Prompt doesn't exist",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        await State.clear();
+        await interaction.reply({
+            content: "Prompt deleted",
+            ephemeral: true,
+        });
+    },
 });
