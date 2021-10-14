c477809c18f2f6657c2e528df6f071a8f16be557
Cabidge <cabbageboi69@gmail.com>
2021-10-13 20:34:14 -0400

    Created single State object to be shared across prompt subcommands

--- src/commands/prompt/create.ts
+++ src/commands/prompt/create.ts
@@ -1,14 +1,7 @@
-import {
-    GuildChannel,
-    MessageActionRow,
-    MessageEmbed,
-    MessageSelectMenu,
-} from "discord.js";
+import { GuildChannel } from "discord.js";
 import { OptionType, slashLeaf } from "../../slash-command";
-import { initStatus, ReportResult } from "./shared/status";
-import { AsyncTaskQueue } from "./shared/async-action-queue";
-import { AntiSpam } from "./shared/anti-spam";
 import { adminOnly } from "../../slash-command/guards";
+import State from "./shared/state";
 
 export default slashLeaf({
     name: "create",
@@ -37,112 +30,14 @@ export default slashLeaf({
             return;
         }
 
-        const status = initStatus();
-
-        const createSelectRow = (
-            escalators: [number, number][],
-            placeholder: string,
-            id: string
-        ) =>
-            new MessageActionRow().addComponents(
-                new MessageSelectMenu()
-                    .setPlaceholder(placeholder)
-                    .setCustomId(id)
-                    .addOptions(
-                        escalators.map(([a, b]) => ({
-                            label: `${a} to ${b}`,
-                            value: `${a}${b}`,
-                        }))
-                    )
-            );
-
-        const createComponents = () => {
-            const { woke, broke } = status.splitStatuses;
-            const components: MessageActionRow[] = [];
-            if (woke.length > 0)
-                components.push(
-                    createSelectRow(woke, "Escalator machine broke", "report")
-                );
-            if (broke.length > 0)
-                components.push(
-                    createSelectRow(
-                        broke,
-                        "Escalator has been revived",
-                        "resolve"
-                    )
-                );
-            return components;
-        };
-
-        await interaction.reply({
-            content: status.message,
-            components: createComponents(),
-        });
-
-        const taskQueue = new AsyncTaskQueue();
-        const antiSpam = new AntiSpam(10_000);
-
-        const collector =
-            interaction.channel!.createMessageComponentCollector();
-        collector.on("collect", (i) => {
-            if (!i.isSelectMenu()) return;
-
-            taskQueue.enqueue(async () => {
-                const selected = i.values[0];
-                const start = parseInt(selected[0]);
-                const end = parseInt(selected[1]);
-
-                const { user } = i;
-                if (!antiSpam.tryInteract(user.id)) {
-                    await user.send(
-                        "Chill out! To prevent spam, you must wait 10 seconds between each report"
-                    );
-                    return;
-                }
-
-                const isBroke = i.customId === "report";
-
-                const result = (isBroke ? status.report : status.resolve)(
-                    start,
-                    end
-                );
-
-                switch (result) {
-                    case ReportResult.Success:
-                        await i.update({
-                            content: status.message,
-                            components: createComponents(),
-                        });
-
-                        if (historyChannel) {
-                            const embed = new MessageEmbed()
-                                .setColor(isBroke ? "RED" : "GREEN")
-                                .setAuthor(
-                                    `reported by ${user.tag}`,
-                                    user.avatarURL() ?? undefined
-                                )
-                                .setTitle(
-                                    `${start}-${end} is ${
-                                        isBroke ? "down" : "back and running"
-                                    }!`
-                                );
-                            await historyChannel.send({ embeds: [embed] });
-                        }
-                        break;
-                    case ReportResult.Invalid:
-                        await i.reply({
-                            content: `The ${start} to ${end} doesn't exist, silly`,
-                            ephemeral: true,
-                        });
-                        break;
-                    case ReportResult.Redundant:
-                        await i.reply({
-                            content:
-                                "Whoops! It seems like someone already reported this before you.",
-                            ephemeral: true,
-                        });
-                }
+        if (State.status !== undefined) {
+            await interaction.reply({
+                content: "Prompt already exists",
+                ephemeral: true,
             });
-        });
+            return;
+        }
+
+        await State.create(interaction, historyChannel ?? undefined);
     },
 });
---
+++ src/commands/prompt/shared/state.ts
@@ -0,0 +1,153 @@
+import {
+    CommandInteraction,
+    InteractionCollector,
+    Message,
+    MessageComponentInteraction,
+    MessageEmbed,
+    NewsChannel,
+    TextChannel,
+} from "discord.js";
+import { createStatusBody, initStatus, ReportResult, Status } from "./status";
+import { AsyncTaskQueue } from "./async-action-queue";
+import { AntiSpam } from "./anti-spam";
+
+interface EscalatorState {
+    readonly status?: Status;
+    create(
+        interaction: CommandInteraction,
+        historyChannel?: TextChannel | NewsChannel
+    ): Promise<void>;
+    reset(): Promise<void>;
+    clear(): Promise<void>;
+}
+
+interface PrivateState {
+    status: Status;
+    promptMessage: Message;
+    collector: InteractionCollector<MessageComponentInteraction>;
+    history?: TextChannel;
+}
+
+let state: PrivateState | undefined;
+
+async function reset() {
+    if (state === undefined) return;
+
+    state.status = initStatus();
+    const { promptMessage, status, history } = state;
+
+    await promptMessage.edit(createStatusBody(status));
+
+    if (history) {
+        const embed = new MessageEmbed()
+            .setColor("GREEN")
+            .setTitle("All escalator statuses have been reset");
+
+        await history.send({ embeds: [embed] });
+    }
+}
+
+async function create(interaction: CommandInteraction, history?: TextChannel) {
+    if (state !== undefined) return;
+
+    const collector = interaction.channel!.createMessageComponentCollector();
+    const status = initStatus();
+
+    const promptMessage = (await interaction.reply({
+        ...createStatusBody(status),
+        fetchReply: true,
+    })) as Message;
+
+    state = {
+        status,
+        collector,
+        promptMessage,
+        history,
+    };
+
+    const taskQueue = new AsyncTaskQueue();
+    const antiSpam = new AntiSpam(10_000);
+
+    collector.on("collect", (i) => {
+        if (!i.isSelectMenu()) return;
+
+        taskQueue.enqueue(async () => {
+            if (state === undefined) {
+                console.error("State not found");
+                return;
+            }
+
+            const selected = i.values[0];
+            const start = parseInt(selected[0]);
+            const end = parseInt(selected[1]);
+
+            const { status, history } = state;
+
+            const { user } = i;
+            if (!antiSpam.tryInteract(user.id)) {
+                await user.send(
+                    "Chill out! To prevent spam, you must wait 10 seconds between each report"
+                );
+                return;
+            }
+
+            const isBroke = i.customId === "report";
+
+            const result = (isBroke ? status.report : status.resolve)(
+                start,
+                end
+            );
+
+            switch (result) {
+                case ReportResult.Success:
+                    await i.update(createStatusBody(status));
+
+                    if (history) {
+                        const embed = new MessageEmbed()
+                            .setColor(isBroke ? "RED" : "GREEN")
+                            .setAuthor(
+                                `reported by ${user.tag}`,
+                                user.avatarURL() ?? undefined
+                            )
+                            .setTitle(
+                                `${start}-${end} is ${
+                                    isBroke ? "down" : "back and running"
+                                }!`
+                            );
+                        await history.send({ embeds: [embed] });
+                    }
+                    break;
+                case ReportResult.Invalid:
+                    await i.reply({
+                        content: `The ${start} to ${end} doesn't exist, silly`,
+                        ephemeral: true,
+                    });
+                    break;
+                case ReportResult.Redundant:
+                    await i.reply({
+                        content:
+                            "Whoops! It seems like someone already reported this before you.",
+                        ephemeral: true,
+                    });
+            }
+        });
+    });
+}
+
+async function clear() {
+    if (state === undefined) return;
+
+    state.collector.stop("deletePrompt");
+    await state.promptMessage.delete();
+
+    state = undefined;
+}
+
+export default {
+    get status() {
+        return state?.status;
+    },
+    reset,
+    create,
+    clear,
+} as EscalatorState;
