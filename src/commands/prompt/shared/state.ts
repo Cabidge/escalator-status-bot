cc19d0a5a8de4557dd04890d9246ecc5a849338d
Cabidge <cabbageboi69@gmail.com>
2021-10-18 19:45:24 -0400

    Made escalator state a promise and set up repoJson var

--- src/commands/prompt/shared/state.ts
+++ src/commands/prompt/shared/state.ts
@@ -1,5 +1,4 @@
 import {
-    CommandInteraction,
     InteractionCollector,
     Message,
     MessageComponentInteraction,
@@ -10,6 +9,8 @@ import {
 import { createStatusBody, initStatus, ReportResult, Status } from "./status";
 import { AsyncTaskQueue } from "./async-action-queue";
 import { AntiSpam } from "./anti-spam";
+import { readyClient } from "../../..";
+import { RepoJson } from "../../../ghdb";
 
 interface EscalatorState {
     readonly status: Status;
@@ -25,136 +26,149 @@ interface PromptState {
     collector: InteractionCollector<MessageComponentInteraction>;
 }
 
-let status = initStatus();
+const repoJson = new RepoJson({
+    token: process.env.ESC_GH_TOKEN!,
+    userAgent: "escalator-status-bot",
+    owner: "Cabidge",
+    repo: "escalator-status-db",
+    path: "state.json",
+    defaultObj: {},
+});
 
-let prompt: PromptState | undefined;
-let history: TextChannel | NewsChannel | undefined;
+export default (async () => {
+    const client = await readyClient;
 
-async function reset() {
-    status = initStatus();
+    let status = initStatus();
 
-    if (prompt) {
-        await prompt.message.edit(createStatusBody(status));
+    let prompt: PromptState | undefined;
+    let history: TextChannel | NewsChannel | undefined;
+
+    async function reset() {
+        status = initStatus();
+
+        if (prompt) {
+            await prompt.message.edit(createStatusBody(status));
+        }
+
+        if (history) {
+            const embed = new MessageEmbed()
+                .setColor("GREEN")
+                .setTitle("All escalator statuses have been reset");
+
+            await history.send({ embeds: [embed] });
+        }
     }
 
-    if (history) {
-        const embed = new MessageEmbed()
-            .setColor("GREEN")
-            .setTitle("All escalator statuses have been reset");
+    async function bind(message: Message) {
+        if (prompt) return;
 
-        await history.send({ embeds: [embed] });
-    }
-}
+        message = await message.edit(createStatusBody(status));
+        const collector = message.channel.createMessageComponentCollector();
 
-async function bind(message: Message) {
-    if (prompt) return;
+        prompt = { message, collector };
 
-    message = await message.edit(createStatusBody(status));
-    const collector = message.channel.createMessageComponentCollector();
+        const taskQueue = new AsyncTaskQueue();
+        const antiSpam = new AntiSpam(10_000);
 
-    prompt = { message, collector };
+        collector.on("collect", (i) => {
+            if (!i.isSelectMenu()) return;
 
-    const taskQueue = new AsyncTaskQueue();
-    const antiSpam = new AntiSpam(10_000);
+            taskQueue.enqueue(async () => {
+                const now = new Date();
+                const today = now.getDay();
+                // If today is Saturday or Sunday
+                if ([0, 6].includes(today)) {
+                    await i.user.send(
+                        "Sorry, status reports are only active during school days"
+                    );
+                    return;
+                }
 
-    collector.on("collect", (i) => {
-        if (!i.isSelectMenu()) return;
+                const hour = now.getHours();
+                // If the time is not between 7am and 5pm
+                if (hour < 7 || hour > 17) {
+                    await i.user.send(
+                        "Sorry, status reports are only active during school hours"
+                    );
+                    return;
+                }
 
-        taskQueue.enqueue(async () => {
-            const now = new Date();
-            const today = now.getDay();
-            // If today is Saturday or Sunday
-            if ([0, 6].includes(today)) {
-                await i.user.send(
-                    "Sorry, status reports are only active during school days"
+                const selected = i.values[0];
+                const start = parseInt(selected[0]);
+                const end = parseInt(selected[1]);
+
+                const { user } = i;
+                if (!antiSpam.tryInteract(user.id)) {
+                    await user.send(
+                        "Chill out! To prevent spam, you must wait 10 seconds between each report"
+                    );
+                    return;
+                }
+
+                const isBroke = i.customId === "report";
+
+                const result = (isBroke ? status.report : status.resolve)(
+                    start,
+                    end
                 );
-                return;
-            }
 
-            const hour = now.getHours();
-            // If the time is not between 7am and 5pm
-            if (hour < 7 || hour > 17) {
-                await i.user.send(
-                    "Sorry, status reports are only active during school hours"
-                );
-                return;
-            }
+                switch (result) {
+                    case ReportResult.Success:
+                        await i.update(createStatusBody(status));
 
-            const selected = i.values[0];
-            const start = parseInt(selected[0]);
-            const end = parseInt(selected[1]);
-
-            const { user } = i;
-            if (!antiSpam.tryInteract(user.id)) {
-                await user.send(
-                    "Chill out! To prevent spam, you must wait 10 seconds between each report"
-                );
-                return;
-            }
-
-            const isBroke = i.customId === "report";
-
-            const result = (isBroke ? status.report : status.resolve)(
-                start,
-                end
-            );
-
-            switch (result) {
-                case ReportResult.Success:
-                    await i.update(createStatusBody(status));
-
-                    if (history) {
-                        const embed = new MessageEmbed()
-                            .setColor(isBroke ? "RED" : "GREEN")
-                            .setAuthor(
-                                `reported by ${user.tag}`,
-                                user.avatarURL() ?? undefined
-                            )
-                            .setTitle(
-                                `${start}-${end} is ${
-                                    isBroke ? "down" : "back and running"
-                                }!`
-                            );
-                        await history.send({ embeds: [embed] });
-                    }
-                    break;
-                case ReportResult.Invalid:
-                    await i.reply({
-                        content: `The ${start} to ${end} doesn't exist, silly`,
-                        ephemeral: true,
-                    });
-                    break;
-                case ReportResult.Redundant:
-                    await i.reply({
-                        content:
-                            "Whoops! It seems like someone already reported this before you.",
-                        ephemeral: true,
-                    });
-            }
+                        if (history) {
+                            const embed = new MessageEmbed()
+                                .setColor(isBroke ? "RED" : "GREEN")
+                                .setAuthor(
+                                    `reported by ${user.tag}`,
+                                    user.avatarURL() ?? undefined
+                                )
+                                .setTitle(
+                                    `${start}-${end} is ${
+                                        isBroke ? "down" : "back and running"
+                                    }!`
+                                );
+                            await history.send({ embeds: [embed] });
+                        }
+                        break;
+                    case ReportResult.Invalid:
+                        await i.reply({
+                            content: `The ${start} to ${end} doesn't exist, silly`,
+                            ephemeral: true,
+                        });
+                        break;
+                    case ReportResult.Redundant:
+                        await i.reply({
+                            content:
+                                "Whoops! It seems like someone already reported this before you.",
+                            ephemeral: true,
+                        });
+                }
+            });
         });
-    });
-}
+    }
 
-async function clear() {
-    if (prompt === undefined) return;
+    async function clear() {
+        if (prompt === undefined) return;
 
-    prompt.collector.stop("deletePrompt");
-    await prompt.message.delete();
+        prompt.collector.stop("deletePrompt");
+        await prompt.message.delete();
 
-    prompt = undefined;
-}
+        prompt = undefined;
+    }
 
-export default {
-    get status() {
-        return status;
-    },
-    set history(value: TextChannel | NewsChannel | undefined) {
-        history = value;
-    },
-    get isActive() {
-        return prompt !== undefined;
-    },
-    bind,
-    reset,
-    clear,
-} as EscalatorState;
+    return {
+        get status() {
+            return status;
+        },
+        set history(value: TextChannel | NewsChannel | undefined) {
+            history = value;
+        },
+        get isActive() {
+            return prompt !== undefined;
+        },
+        bind,
+        reset,
+        clear,
+    } as EscalatorState;
+})();
